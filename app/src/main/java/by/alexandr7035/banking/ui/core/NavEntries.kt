17248f0555061132753cd8db670df355574e87b0
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2023-09-29 14:21:16 +0300

    Add home bottom nav

---
+++ app/src/main/java/by/alexandr7035/banking/ui/core/NavEntries.kt
@@ -0,0 +1,72 @@
+package by.alexandr7035.banking.ui.core
+
+import by.alexandr7035.banking.R
+
+sealed class NavEntries(
+    val route: String,
+    val navIcons: NavIconPair?,
+    val label: String,
+) {
+
+    object Wizard: NavEntries(
+        route = "wizard",
+        navIcons = null,
+        label = "wizard",
+    )
+
+    object Login: NavEntries(
+        route = "login",
+        navIcons = null,
+        label = "Login",
+    )
+
+
+    object Profile: NavEntries(
+        route = "profile",
+        navIcons = NavIconPair(
+            unselected = R.drawable.ic_profile,
+            selected = R.drawable.ic_profile_filled
+        ),
+        label = "Profile",
+    )
+
+    object Statistics: NavEntries(
+        route = "statistics",
+        navIcons = NavIconPair(
+            unselected = R.drawable.ic_statistics,
+            selected = R.drawable.ic_statistics_selected
+        ),
+        label = "Statistics",
+    )
+
+    object Home: NavEntries(
+        route = "home",
+        navIcons = NavIconPair(
+            unselected = R.drawable.ic_home,
+            selected = R.drawable.ic_home_selected
+        ),
+        label = "Home",
+    )
+
+    object History: NavEntries(
+        route = "history",
+        navIcons = NavIconPair(
+            unselected = R.drawable.ic_history,
+            selected = R.drawable.ic_history_selected
+        ),
+        label = "History",
+    )
+
+    sealed class Graphs(val route: String) {
+        object HomeGraph: Graphs("homeGraph")
+    }
+
+    companion object {
+        val primaryDestinations = listOf(
+            Home.route,
+            History.route,
+            Statistics.route,
+            Profile.route,
+        )
+    }
+}
