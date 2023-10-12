181529fb6bbecaee801d4d31f59604f4e5a2fcdc
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2023-10-12 15:08:26 +0300

    Add extension

---
+++ app/src/main/java/by/alexandr7035/banking/ui/extensions/TimeExtensions.kt
@@ -0,0 +1,9 @@
+package by.alexandr7035.banking.ui.extensions
+
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+fun Long.getFormattedDate(format: String): String {
+    val formatter = SimpleDateFormat(format, Locale.US)
+    return formatter.format(this)
+}
