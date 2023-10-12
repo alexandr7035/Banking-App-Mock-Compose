7e8cbbb0a494df5c2b66592fbb112a33474e29c9
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2023-10-13 01:49:55 +0300

    Refactoring of intents

--- app/src/main/java/by/alexandr7035/banking/ui/feature_cards/screen_add_card/AddCardIntent.kt
+++ app/src/main/java/by/alexandr7035/banking/ui/feature_cards/screen_add_card/AddCardIntent.kt
@@ -2,12 +2,11 @@ package by.alexandr7035.banking.ui.feature_cards.screen_add_card
 
 sealed class AddCardIntent {
     object EnterScreen : AddCardIntent()
-    data class CardNumberChanged(val number: String) : AddCardIntent()
-    data class CvvCodeChanged(val code: String) : AddCardIntent()
-    data class ExpirationDateChanged(val date: Long?) : AddCardIntent()
-    data class CardHolderChanged(val cardHolder: String) : AddCardIntent()
-    data class AddressFirstLineChanged(val addressLine: String) : AddCardIntent()
-    data class AddressSecondLineChanged(val addressLine: String) : AddCardIntent()
+    data class StringFieldChanged(
+        val fieldType: AddCardFieldType,
+        val fieldValue: String
+    ) : AddCardIntent()
+    data class ExpirationPickerSet(val date: Long?) : AddCardIntent()
     object SaveCard : AddCardIntent()
     object ConsumeResultEvent : AddCardIntent()
     data class ToggleDatePicker(val isShown: Boolean) : AddCardIntent()
