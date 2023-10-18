281ab18b0311fb01afec715c13a7d2b595fcd510
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2023-10-19 01:51:27 +0300

    Setup splashscreen
    #17

--- app/src/main/java/by/alexandr7035/banking/MainActivity.kt
+++ app/src/main/java/by/alexandr7035/banking/MainActivity.kt
@@ -7,11 +7,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import by.alexandr7035.banking.ui.core.AppContainerScreen
 import by.alexandr7035.banking.ui.theme.BankingAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             BankingAppTheme {
--- app/src/main/java/by/alexandr7035/banking/ui/theme/Theme.kt
+++ app/src/main/java/by/alexandr7035/banking/ui/theme/Theme.kt
@@ -60,6 +60,9 @@ fun BankingAppTheme(
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+
+            window.navigationBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars = !darkTheme
         }
     }
 
