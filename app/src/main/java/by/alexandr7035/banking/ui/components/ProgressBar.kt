6351a7837f2c17b66413a73254646115282cebad
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2023-10-19 00:40:32 +0300

    Add animated DotsProgressIndicator

--- app/src/main/java/by/alexandr7035/banking/ui/components/ProgressBar.kt
+++ app/src/main/java/by/alexandr7035/banking/ui/components/ProgressBar.kt
@@ -1,23 +1,35 @@
 package by.alexandr7035.banking.ui.components
 
 import AutoSizeText
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.keyframes
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -26,6 +38,7 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import by.alexandr7035.banking.ui.theme.BankingAppTheme
 import by.alexandr7035.banking.ui.theme.primaryFontFamily
+import kotlinx.coroutines.delay
 import kotlin.math.floor
 import kotlin.math.roundToInt
 
@@ -116,6 +129,66 @@ fun PercentageIndicator(
     }
 }
 
+@Composable
+fun DotsProgressIndicator(
+    modifier: Modifier = Modifier,
+    circleSize: Dp = 24.dp,
+    spaceBetween: Dp = 12.dp,
+    travelDistance: Dp = 20.dp,
+    circleColor: Color = MaterialTheme.colorScheme.primary
+) {
+    val circles = listOf(
+        remember { Animatable(0f) },
+        remember { Animatable(0f) },
+        remember { Animatable(0f) },
+    )
+
+    // Actual values during animation
+    val circleValues = circles.map { it.value }
+    val distance = with(LocalDensity.current) { travelDistance.toPx() }
+
+    // Launch animation
+    circles.forEachIndexed { index, animatable ->
+        LaunchedEffect(key1 = animatable) {
+            delay(index * 100L)
+            animatable.animateTo(
+                targetValue = 1f,
+                animationSpec = infiniteRepeatable(
+                    animation = keyframes {
+                        durationMillis = 1200
+                        0.0f at 0 with LinearOutSlowInEasing
+                        1.0f at 300 with LinearOutSlowInEasing
+                        0.0f at 600 with LinearOutSlowInEasing
+                        0.0f at 1200 with LinearOutSlowInEasing
+                    },
+                    repeatMode = RepeatMode.Restart
+                )
+            )
+        }
+    }
+
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(spaceBetween)
+    ) {
+        circleValues.forEachIndexed { index, value ->
+            Box(
+                Modifier
+                    .size(circleSize)
+                    .graphicsLayer {
+                        translationY = -value * distance
+                    }
+                    .background(
+                        shape = CircleShape,
+                        color = circleColor
+                    )
+            )
+        }
+    }
+
+
+}
+
 @Preview
 @Composable
 fun PercentageIndicator_Preview() {
@@ -135,5 +208,19 @@ fun PercentageIndicator_Preview() {
 fun FullscreenProgressBar_Preview() {
     ScreenPreview {
         FullscreenProgressBar()
+    }
+}
+
+@Preview
+@Composable
+fun DotsProgressIndicator_Preview() {
+    BankingAppTheme() {
+        Surface(
+            Modifier
+                .background(MaterialTheme.colorScheme.background)
+                .padding(16.dp)
+        ) {
+            DotsProgressIndicator()
+        }
     }
 }
