1d03d1e45f1e8ec5262d311135612065fad1cda9
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2023-10-25 19:18:44 +0300

    Update error screen ui

--- app/src/main/java/by/alexandr7035/banking/ui/components/error/ErrorFullScreen.kt
+++ app/src/main/java/by/alexandr7035/banking/ui/components/error/ErrorFullScreen.kt
@@ -2,22 +2,29 @@ package by.alexandr7035.banking.ui.components.error
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import by.alexandr7035.banking.R
 import by.alexandr7035.banking.ui.components.PrimaryButton
 import by.alexandr7035.banking.ui.core.resources.UiText
 import by.alexandr7035.banking.ui.theme.BankingAppTheme
+import by.alexandr7035.banking.ui.theme.primaryFontFamily
 
 
 @Composable
@@ -26,50 +33,64 @@ fun ErrorFullScreen(
     modifier: Modifier = Modifier,
     onRetry: (() -> Unit)? = null,
 ) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxSize()
-            .then(modifier)
-    ) {
 
-        Image(
-            painter = painterResource(id = R.drawable.ic_error),
-            contentDescription = null,
-            modifier = Modifier.size(100.dp)
-        )
+    BoxWithConstraints(Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .height(maxHeight)
+                .width(maxWidth)
+                .verticalScroll(rememberScrollState())
+                .padding(
+                    vertical = 40.dp,
+                    horizontal = 24.dp
+                ),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(Modifier.height(72.dp))
 
-        Spacer(modifier = Modifier.height(24.dp))
-
-        Text(
-            text = stringResource(R.string.error_happened),
-            style = MaterialTheme.typography.titleLarge,
-            color = MaterialTheme.colorScheme.onBackground,
-            textAlign = TextAlign.Center,
-            fontWeight = FontWeight.Bold
-        )
-
-        Spacer(modifier = Modifier.height(12.dp))
-
-        Text(
-            text = error.asString(),
-            style = MaterialTheme.typography.bodyLarge,
-            color = MaterialTheme.colorScheme.onBackground,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.padding(horizontal = 36.dp)
-        )
-
-        if (onRetry != null) {
-            Spacer(modifier = Modifier.height(16.dp))
-
-            PrimaryButton(
-                onClick = { onRetry.invoke() },
-                text = stringResource(R.string.try_again),
-                modifier = Modifier
-                    .wrapContentSize()
-                    .padding(horizontal = 20.dp)
+            Text(
+                text = stringResource(R.string.error_happened),
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontFamily = primaryFontFamily,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Color(0xFF333333),
+                    textAlign = TextAlign.Center,
+                )
             )
+
+            Spacer(Modifier.height(16.dp))
+
+            Text(
+                text = error.asString(),
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    lineHeight = 24.sp,
+                    fontFamily = primaryFontFamily,
+                    fontWeight = FontWeight.Normal,
+                    color = Color(0xFF999999),
+                    textAlign = TextAlign.Center,
+                )
+            )
+
+            Spacer(Modifier.weight(1f))
+
+            Image(
+                painter = painterResource(id = R.drawable.img_error),
+                contentDescription = "Error image",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.size(200.dp)
+            )
+
+            Spacer(Modifier.weight(1f))
+
+            if (onRetry != null) {
+                PrimaryButton(
+                    modifier = Modifier.fillMaxSize(),
+                    text = stringResource(R.string.try_again),
+                    onClick = onRetry
+                )
+            }
         }
     }
 }
