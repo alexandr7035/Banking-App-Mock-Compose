e65cb9f9865496ea6b08659ca8f220aa3dca481b
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2023-10-30 19:14:37 +0300

    Add context extensions

--- app/src/main/java/by/alexandr7035/banking/ui/core/extensions/ContextExtensions.kt
+++ app/src/main/java/by/alexandr7035/banking/ui/core/extensions/ContextExtensions.kt
@@ -1,8 +1,27 @@
 package by.alexandr7035.banking.ui.core.extensions
 
 import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.os.Build
+import android.provider.Settings
 import android.widget.Toast
+import androidx.fragment.app.FragmentActivity
 
 fun Context.showToast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun Context.findActivity(): FragmentActivity? = when (this) {
+    is FragmentActivity -> this
+    is ContextWrapper -> baseContext.findActivity()
+    else -> null
+}
+
+fun Context.openBiometricsSettings() {
+    val intent = when {
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> Intent(Settings.ACTION_BIOMETRIC_ENROLL)
+        else -> Intent(Settings.ACTION_SECURITY_SETTINGS)
+    }
+    startActivity(intent)
 }
