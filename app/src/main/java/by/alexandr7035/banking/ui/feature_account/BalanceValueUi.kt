48b36ba73b4e32c67c581953f34abef175b520c4
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2023-11-01 21:20:09 +0300

    Update balance representation classes

--- app/src/main/java/by/alexandr7035/banking/ui/feature_account/BalanceValueUi.kt
+++ app/src/main/java/by/alexandr7035/banking/ui/feature_account/BalanceValueUi.kt
@@ -1,28 +1,26 @@
 package by.alexandr7035.banking.ui.feature_account
 
 import by.alexandr7035.banking.domain.features.account.model.BalanceCurrency
-import by.alexandr7035.banking.domain.features.account.model.BalanceValue
-import kotlin.math.roundToInt
+import by.alexandr7035.banking.domain.features.account.model.MoneyAmount
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
+import java.util.Locale
 
 data class BalanceValueUi(
     val balanceStr: String,
 ) {
     companion object {
-        fun mapFromDomain(balance: BalanceValue): BalanceValueUi {
-            val balanceStr = when (balance) {
-                is BalanceValue.DoubleBalance -> {
-                    val rounded = (balance.value * 100.0).roundToInt() / 100.0
-                    "$rounded"
-                }
-                is BalanceValue.LongBalance -> {
-                    "${balance.value}"
-                }
-            }
+        fun mapFromDomain(balance: MoneyAmount): BalanceValueUi {
+            val symbols = DecimalFormatSymbols(Locale.US)
+            val decimalFormat = DecimalFormat("#,##0.##", symbols)
+            decimalFormat.isGroupingUsed = false
+            val formattedValue = decimalFormat.format(balance.value)
+
 
             // Add currency prefixes
             return when (balance.currency) {
                 BalanceCurrency.USD -> {
-                    BalanceValueUi("$$balanceStr")
+                    BalanceValueUi("$$formattedValue")
                 }
             }
         }
