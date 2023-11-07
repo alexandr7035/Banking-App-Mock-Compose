0e245b087be7008cac1d67e9fa69883fb0dbd67f
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2023-11-07 15:37:16 +0300

    Add sumFloat extension

---
+++ app/src/main/java/by/alexandr7035/banking/core/extensions/CollectionExtensions.kt
@@ -0,0 +1,9 @@
+package by.alexandr7035.banking.core.extensions
+
+inline fun <T> Iterable<T>.sumFloat(selector: (T) -> Float): Float {
+    var sum: Float = 0F
+    for (element in this) {
+        sum += selector(element)
+    }
+    return sum
+}
