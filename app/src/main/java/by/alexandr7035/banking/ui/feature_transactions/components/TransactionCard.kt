029a61c470694b5437b49d2896ae4aa8b336716f
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2023-11-15 02:29:48 +0300

    Update TransactionUi

--- app/src/main/java/by/alexandr7035/banking/ui/feature_transactions/components/TransactionCard.kt
+++ app/src/main/java/by/alexandr7035/banking/ui/feature_transactions/components/TransactionCard.kt
@@ -1,5 +1,6 @@
 package by.alexandr7035.banking.ui.feature_transactions.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -10,6 +11,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -21,13 +23,18 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import by.alexandr7035.banking.R
+import by.alexandr7035.banking.domain.features.transactions.model.TransactionStatus
+import by.alexandr7035.banking.domain.features.transactions.model.TransactionType
 import by.alexandr7035.banking.ui.components.debug.debugPlaceholder
 import by.alexandr7035.banking.ui.feature_transactions.model.TransactionUi
 import by.alexandr7035.banking.ui.theme.BankingAppTheme
@@ -38,99 +45,182 @@ import coil.request.ImageRequest
 
 @Composable
 fun TransactionCard(
-    modifier: Modifier = Modifier,
-    transactionUi: TransactionUi
+    modifier: Modifier = Modifier, transactionUi: TransactionUi
 ) {
 
     val shape = RoundedCornerShape(10.dp)
 
-    Box(
-        modifier = modifier.then(
-            Modifier
-                .shadow(
-                    elevation = 32.dp,
-                    spotColor = Color.Gray,
-                    ambientColor = Color.Gray,
-                    shape = shape,
-                )
-                .background(
-                    color = Color.White, shape = shape
-                )
-                .clickable {
-//                    onClick.invoke(savingUi.id)
-                }
-                .padding(16.dp)
+    Box(modifier = modifier.then(Modifier
+        .shadow(
+            elevation = 32.dp,
+            spotColor = Color.Gray,
+            ambientColor = Color.Gray,
+            shape = shape,
         )
-    ) {
-        Row(
-            modifier = Modifier.height(IntrinsicSize.Max),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            // TODO separate ui with // without contact
-            val imageReq = ImageRequest.Builder(LocalContext.current)
-                .data(transactionUi.contact?.profilePictureUrl)
-                .decoderFactory(SvgDecoder.Factory())
-                .crossfade(true)
-                .build()
+        .background(
+            color = Color.White, shape = shape
+        )
+        .clickable {
+//                    onClick.invoke(savingUi.id)
+        }
+        .padding(16.dp))) {
 
+        when (transactionUi.type) {
+            TransactionType.TOP_UP -> TopUpTransactionCard(transactionUi = transactionUi)
+            else -> ContactTransactionCard(transactionUi = transactionUi)
+        }
+
+    }
+}
+
+@Composable
+private fun ContactTransactionCard(
+    transactionUi: TransactionUi
+) {
+    Row(
+        modifier = Modifier.height(IntrinsicSize.Max), verticalAlignment = Alignment.CenterVertically
+    ) {
+        // TODO placeholder
+        val imageReq =
+            ImageRequest.Builder(LocalContext.current).data(transactionUi.contact?.profilePictureUrl).decoderFactory(SvgDecoder.Factory())
+                .crossfade(true).build()
+
+        Box() {
             AsyncImage(
-                model = imageReq,
+                model = imageReq, modifier = Modifier
+                    .background(
+                        color = Color(0xFFF2F2F2),
+                        shape = CircleShape,
+                    )
+                    .size(48.dp)
+                    .padding(8.dp), contentDescription = null, placeholder = debugPlaceholder(debugPreview = R.drawable.ic_home)
+            )
+
+            Image(
+                modifier = Modifier
+                    .size(16.dp)
+                    .offset(0.dp, -4.dp)
+                    .align(Alignment.TopEnd),
+                painter = getTxStatusMark(transactionUi),
+                contentDescription = "${transactionUi.status}"
+            )
+        }
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        Column(
+            verticalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxHeight()
+                .weight(1f, fill = true),
+        ) {
+
+            Text(
+                text = transactionUi.contact?.name ?: stringResource(R.string.unknown_user), style = TextStyle(
+                    fontSize = 14.sp,
+                    lineHeight = 20.sp,
+                    fontFamily = primaryFontFamily,
+                    fontWeight = FontWeight(500),
+                    color = Color(0xFF333333),
+                )
+            )
+
+            Text(
+                text = transactionUi.transactionDate, style = TextStyle(
+                    fontSize = 12.sp,
+                    fontFamily = primaryFontFamily,
+                    fontWeight = FontWeight(400),
+                    color = Color(0xFF100D40),
+                )
+            )
+        }
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        Text(
+            text = getTxValueWithPrefix(transactionUi), style = TextStyle(
+                fontSize = 14.sp,
+                lineHeight = 20.sp,
+                fontFamily = primaryFontFamily,
+                fontWeight = FontWeight(500),
+                color = Color(0xFF100D40),
+            )
+        )
+    }
+}
+
+@Composable
+private fun TopUpTransactionCard(
+    transactionUi: TransactionUi,
+) {
+    Row(
+        modifier = Modifier.height(IntrinsicSize.Max), verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Box() {
+            Image(
+                painter = painterResource(id = R.drawable.ic_topup_arrow),
+                contentDescription = "TopUp transaction",
                 modifier = Modifier
                     .background(
                         color = Color(0xFFF2F2F2),
                         shape = CircleShape,
                     )
                     .size(48.dp)
-                    .padding(8.dp),
-                contentDescription = null,
-                placeholder = debugPlaceholder(debugPreview = R.drawable.ic_home)
+                    .padding(8.dp)
             )
 
-            Spacer(modifier = Modifier.width(16.dp))
-
-            Column(
-                verticalArrangement = Arrangement.SpaceBetween,
+            Image(
                 modifier = Modifier
-                    .fillMaxHeight()
-                    .weight(1f, fill = true),
-            ) {
+                    .size(16.dp)
+                    .offset(0.dp, -4.dp)
+                    .align(Alignment.TopEnd),
+                painter = getTxStatusMark(transactionUi),
+                contentDescription = "${transactionUi.status}"
+            )
+        }
 
-                // FIXME
-                Text(
-                    text =  transactionUi.contact?.name ?: "TODO",
-                    style = TextStyle(
-                        fontSize = 14.sp,
-                        lineHeight = 20.sp,
-                        fontFamily = primaryFontFamily,
-                        fontWeight = FontWeight(500),
-                        color = Color(0xFF333333),
-                    )
-                )
 
-                Text(
-                    text = transactionUi.transactionDate,
-                    style = TextStyle(
-                        fontSize = 12.sp,
-                        fontFamily = primaryFontFamily,
-                        fontWeight = FontWeight(400),
-                        color = Color(0xFF100D40),
-                    )
-                )
-            }
 
-            Spacer(modifier = Modifier.width(16.dp))
+        Spacer(modifier = Modifier.width(16.dp))
 
+        Column(
+            verticalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxHeight()
+                .weight(1f, fill = true),
+        ) {
             Text(
-                text = transactionUi.value.amountStr,
-                style = TextStyle(
+                text = stringResource(id = R.string.top_up), style = TextStyle(
                     fontSize = 14.sp,
                     lineHeight = 20.sp,
                     fontFamily = primaryFontFamily,
                     fontWeight = FontWeight(500),
+                    color = Color(0xFF333333),
+                )
+            )
+
+            Text(
+                text = transactionUi.transactionDate, style = TextStyle(
+                    fontSize = 12.sp,
+                    fontFamily = primaryFontFamily,
+                    fontWeight = FontWeight(400),
                     color = Color(0xFF100D40),
                 )
             )
         }
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        Text(
+            text = getTxValueWithPrefix(transactionUi), style = TextStyle(
+                fontSize = 14.sp,
+                lineHeight = 20.sp,
+                fontFamily = primaryFontFamily,
+                fontWeight = FontWeight(500),
+                color = Color(0xFF100D40),
+            )
+        )
     }
 }
 
@@ -138,8 +228,47 @@ fun TransactionCard(
 @Preview
 fun TransactionCardUi_Preview() {
     BankingAppTheme() {
-        TransactionCard(
-            transactionUi = TransactionUi.mock()
-        )
+        Column(verticalArrangement = Arrangement.spacedBy(20.dp)) {
+            TransactionCard(
+                transactionUi = TransactionUi.mock(
+                    type = TransactionType.TOP_UP, status = TransactionStatus.PENDING
+                )
+            )
+
+            TransactionCard(
+                transactionUi = TransactionUi.mock(
+                    type = TransactionType.SEND, status = TransactionStatus.COMPLETED
+                )
+            )
+
+            TransactionCard(
+                transactionUi = TransactionUi.mock(
+                    type = TransactionType.RECEIVE, status = TransactionStatus.FAILED
+                )
+            )
+
+        }
     }
+}
+
+
+private fun getTxValueWithPrefix(
+    transactionUi: TransactionUi
+): String {
+    return when (transactionUi.type) {
+        TransactionType.SEND -> "-${transactionUi.value.amountStr}"
+        TransactionType.RECEIVE -> "+${transactionUi.value.amountStr}"
+        TransactionType.TOP_UP -> "+${transactionUi.value.amountStr}"
+    }
+}
+
+@Composable
+private fun getTxStatusMark(transactionUi: TransactionUi): Painter {
+    val res = when (transactionUi.status) {
+        TransactionStatus.PENDING -> R.drawable.ic_status_pending
+        TransactionStatus.COMPLETED -> R.drawable.ic_status_accepted
+        TransactionStatus.FAILED -> R.drawable.ic_status_rejected
+    }
+
+    return painterResource(id = res)
 }
--- app/src/main/java/by/alexandr7035/banking/ui/feature_transactions/model/TransactionUi.kt
+++ app/src/main/java/by/alexandr7035/banking/ui/feature_transactions/model/TransactionUi.kt
@@ -1,31 +1,43 @@
 package by.alexandr7035.banking.ui.feature_transactions.model
 
 import by.alexandr7035.banking.domain.features.transactions.model.Transaction
+import by.alexandr7035.banking.domain.features.transactions.model.TransactionStatus
+import by.alexandr7035.banking.domain.features.transactions.model.TransactionType
 import by.alexandr7035.banking.ui.core.extensions.getFormattedDate
 import by.alexandr7035.banking.ui.feature_account.MoneyAmountUi
 import by.alexandr7035.banking.ui.feature_contacts.model.ContactUi
 
 data class TransactionUi(
     val id: Long,
+    val type: TransactionType,
+    val status: TransactionStatus,
     val value: MoneyAmountUi,
     val transactionDate: String,
     val contact: ContactUi?
 ) {
     companion object {
-        fun mock(): TransactionUi {
+        fun mock(
+            id: Long = 0,
+            type: TransactionType = TransactionType.SEND,
+            status: TransactionStatus = TransactionStatus.COMPLETED,
+            transactionDate: String = "13 Oct 2021",
+            value: MoneyAmountUi = MoneyAmountUi("$200.50"),
+            contact: ContactUi? = ContactUi.mock()
+        ): TransactionUi {
             return TransactionUi(
-                id = 0,
-                transactionDate = "13 Oct 2021",
-                value = MoneyAmountUi("$200.50"),
-                contact = ContactUi.mock()
+                id = id,
+                type = type,
+                transactionDate = transactionDate,
+                status = status,
+                value = value,
+                contact = contact
             )
         }
 
         fun mapFromDomain(transaction: Transaction): TransactionUi {
             val contact = if (transaction.linkedContact != null) {
                 ContactUi.mapFromDomain(transaction.linkedContact)
-            }
-            else {
+            } else {
                 null
             }
 
@@ -34,6 +46,8 @@ data class TransactionUi(
                 value = MoneyAmountUi.mapFromDomain(transaction.value),
                 transactionDate = transaction.createdDate.getFormattedDate("dd MMM yyyy HH:mm"),
                 contact = contact,
+                type = transaction.type,
+                status = transaction.recentStatus
             )
         }
     }
