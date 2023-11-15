fe982acc84ec0c35d992f58c1ac5057390015046
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2023-11-15 19:18:06 +0300

    Update TransactionWorker

--- app/src/main/java/by/alexandr7035/banking/data/transactions/TransactionWorker.kt
+++ app/src/main/java/by/alexandr7035/banking/data/transactions/TransactionWorker.kt
@@ -16,56 +16,101 @@ import androidx.work.WorkerParameters
 import by.alexandr7035.banking.MainActivity
 import by.alexandr7035.banking.R
 import by.alexandr7035.banking.data.transactions.db.TransactionDao
+import by.alexandr7035.banking.data.transactions.db.TransactionEntity
 import by.alexandr7035.banking.domain.core.AppError
 import by.alexandr7035.banking.domain.core.ErrorType
+import by.alexandr7035.banking.domain.features.account.AccountRepository
 import by.alexandr7035.banking.domain.features.transactions.model.TransactionStatus
-import by.alexandr7035.banking.ui.core.error.asUiTextError
+import by.alexandr7035.banking.domain.features.transactions.model.TransactionType
+import by.alexandr7035.banking.ui.core.notifications.TransactionNotificationHelper
 import kotlinx.coroutines.delay
 
 class TransactionWorker(
     appContext: Context,
     workerParams: WorkerParameters,
-    private val transactionDao: TransactionDao
+    private val transactionDao: TransactionDao,
+    private val accountRepository: AccountRepository
 ): CoroutineWorker(appContext, workerParams) {
+
+    companion object {
+        const val TRANSACTION_ID_KEY = "transaction_id"
+        const val MOCK_TRANSACTION_DELAY = 10000L
+    }
+
     override suspend fun doWork(): Result {
         Log.d("WORKER_TAG", "Test test ")
         return try {
             val transactionId = inputData.getLong(TRANSACTION_ID_KEY, -1)
-            Log.d("WORKER_TAG", "Transaction ${transactionId}")
+            Log.d("WORKER_TAG", "Transaction operation for transaction: $transactionId")
 
             if (transactionId != -1L) {
-                delay(5000)
+                delay(MOCK_TRANSACTION_DELAY)
 
                 val transaction = transactionDao.getTransaction(transactionId) ?: throw AppError(ErrorType.TRANSACTION_NOT_FOUND)
-//                val transaction = transactionDao.getTransactionsFromCache()
-//                Log.d("WORKER_TAG", "TransactionEnt ${transaction}")
-//
-                // Simulate processing delay
+                executeTransaction(transaction = transaction)
+
                 transactionDao.updateTransaction(transaction.copy(
                     recentStatus = TransactionStatus.COMPLETED,
                     updatedStatusDate = System.currentTimeMillis()
                 ))
 
-                showNotification(
-                    title = "Transaction submitted",
-                    message = "Type ${transaction.type} value ${transaction.value}"
+                val notificationUi = TransactionNotificationHelper.successMessage(
+                    context = applicationContext,
+                    transactionType = transaction.type,
+                    cardId = transaction.cardId,
+                    amount = transaction.value
                 )
+
+                showNotification(
+                    title = notificationUi.title,
+                    message = notificationUi.message
+                )
+
                 Result.success()
             } else {
                 Result.failure()
             }
         }
         catch (e: Exception) {
-            showNotification(
-                title = "Transaction failed",
-                message = ErrorType.fromThrowable(e).asUiTextError().asString(applicationContext)
+            val notificationUi = TransactionNotificationHelper.errorMessage(
+                context = applicationContext,
+                error = e
             )
+
+            showNotification(
+                title = notificationUi.title,
+                message = notificationUi.message
+            )
+
             Result.failure()
         }
     }
 
-    companion object {
-        const val TRANSACTION_ID_KEY = "transaction_id"
+    // When set expedited, work will round in ForegroundService bellow Android 12
+    // So here are notification params
+    override suspend fun getForegroundInfo(): ForegroundInfo {
+        val notificationUi = TransactionNotificationHelper.pending()
+
+        return ForegroundInfo(
+            System.currentTimeMillis().toInt(),
+            getNotification(
+                title = notificationUi.title,
+                message = notificationUi.message
+            )
+        )
+    }
+
+    private suspend fun executeTransaction(transaction: TransactionEntity) {
+        when (transaction.type) {
+            TransactionType.SEND -> TODO()
+            TransactionType.RECEIVE -> TODO()
+            TransactionType.TOP_UP -> {
+                accountRepository.topUpCard(
+                    cardId = transaction.cardId,
+                    amount = transaction.value
+                )
+            }
+        }
     }
 
     private fun showNotification(
@@ -90,21 +135,13 @@ class TransactionWorker(
             applicationContext.getString(R.string.NOTIFICATION_CHANNEL_ID))
             .setContentTitle(title)
             .setContentText(message)
+                // FIXME
             .setSmallIcon(R.mipmap.ic_launcher_round)
             .setContentIntent(pendingIntent)
             // Make not dismissible
 //            .setOngoing(true)
             // Show notification immediately (prevent 10 sec delay)
-            .setForegroundServiceBehavior(NotificationCompat.FOREGROUND_SERVICE_IMMEDIATE)
+//            .setForegroundServiceBehavior(NotificationCompat.FOREGROUND_SERVICE_IMMEDIATE)
             .build()
-    }
-
-    // When set expedited, work will round in ForegroundService bellow Android 12
-    // So here are notification params
-    override suspend fun getForegroundInfo(): ForegroundInfo {
-        return ForegroundInfo(
-            0,
-            getNotification("Operation in progress", "Please, wait a minute..."),
-        )
     }
 }
