5e236cf0b282ee9da26a821707725995f8d29bc4
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2023-11-16 15:13:48 +0300

    Add GetRecentContactUseCase

--- app/src/main/java/by/alexandr7035/banking/core/di/domain/ContactsModule.kt
+++ app/src/main/java/by/alexandr7035/banking/core/di/domain/ContactsModule.kt
@@ -2,9 +2,11 @@ package by.alexandr7035.banking.core.di.domain
 
 import by.alexandr7035.banking.domain.features.contacts.GetContactByIdUseCase
 import by.alexandr7035.banking.domain.features.contacts.GetContactsUseCase
+import by.alexandr7035.banking.domain.features.contacts.GetRecentContactUseCase
 import org.koin.dsl.module
 
 val contactsModule = module {
     factory { GetContactsUseCase(get()) }
     factory { GetContactByIdUseCase(get()) }
+    factory { GetRecentContactUseCase(get()) }
 }
