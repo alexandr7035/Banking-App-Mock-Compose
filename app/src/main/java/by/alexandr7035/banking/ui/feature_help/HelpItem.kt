10a0b85565b6d424f77194a8d1e6fd99b7614fdb
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2023-11-18 21:24:35 +0300

    Add help screen

---
+++ app/src/main/java/by/alexandr7035/banking/ui/feature_help/HelpItem.kt
@@ -0,0 +1,6 @@
+package by.alexandr7035.banking.ui.feature_help
+
+data class HelpItem(
+    val title: String,
+    val text: String
+)
---
+++ app/src/main/java/by/alexandr7035/banking/ui/feature_help/HelpScreen.kt
@@ -0,0 +1,159 @@
+package by.alexandr7035.banking.ui.feature_help
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import by.alexandr7035.banking.R
+import by.alexandr7035.banking.ui.components.ScreenPreview
+import by.alexandr7035.banking.ui.components.SecondaryToolBar
+import by.alexandr7035.banking.ui.core.resources.UiText
+import by.alexandr7035.banking.ui.theme.primaryFontFamily
+
+@Composable
+fun HelpScreen(
+    onBack: () -> Unit = {}
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        SecondaryToolBar(
+            onBack = onBack,
+            title = UiText.StringResource(R.string.help_center)
+        )
+
+        val helpItems = listOf(
+            HelpItem(
+                title = "How to1",
+                text = "How to Looong looong text",
+            ),
+            HelpItem(
+                title = "How to2",
+                text = "How to Looong looong text",
+            ),
+            HelpItem(
+                title = "How to3",
+                text = "How to Looong looong text",
+            )
+        )
+
+        val expandedItem = remember {
+            mutableStateOf<HelpItem?>(null)
+        }
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+//            contentPadding = PaddingValues(horizontal = 24.dp),
+//            verticalArrangement = Arrangement.spacedBy(24.dp)
+        ) {
+            items(helpItems) {
+                HelpItemCard(
+                    item = it,
+                    expanded = expandedItem.value == it
+                ) {
+                    expandedItem.value = if (expandedItem.value == it) null else it
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun HelpItemCard(
+    item: HelpItem,
+    expanded: Boolean,
+    onClick: () -> Unit = {}
+) {
+    Column(
+        modifier = Modifier
+            .animateContentSize()
+            .clickable {
+                onClick()
+            }
+            .padding(
+                top = 24.dp,
+                start = 24.dp,
+                end = 24.dp
+            ),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            val titleColor = if (expanded) {
+                Color(0xFF333333)
+            } else {
+                Color(0xFF666666)
+            }
+
+            Text(
+                text = item.title,
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    fontFamily = primaryFontFamily,
+                    fontWeight = FontWeight.Medium,
+                    color = animateColorAsState(targetValue = titleColor).value,
+                )
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Icon(
+                painter = painterResource(id = R.drawable.ic_arrow),
+                contentDescription = null,
+                tint = Color(0xFF666666)
+            )
+        }
+
+        if (expanded) {
+            Text(
+                text = item.text,
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    lineHeight = 26.sp,
+                    fontFamily = primaryFontFamily,
+                    fontWeight = FontWeight(400),
+                    color = Color(0xFF999999),
+                )
+            )
+        }
+
+        HorizontalDivider(
+            thickness = 1.dp,
+            color = Color(0xFFD9D8DC)
+        )
+    }
+}
+
+@Composable
+@Preview
+fun HelpScreen_Preview() {
+    ScreenPreview {
+        HelpScreen()
+    }
+}
