4f6ebd05fcb7cb329d8d1ae90a24482b0f45e818
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2023-11-18 23:08:22 +0300

    Add search on HelpScreen

--- app/src/main/java/by/alexandr7035/banking/ui/feature_help/HelpScreen.kt
+++ app/src/main/java/by/alexandr7035/banking/ui/feature_help/HelpScreen.kt
@@ -3,6 +3,7 @@ package by.alexandr7035.banking.ui.feature_help
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -11,26 +12,34 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import by.alexandr7035.banking.R
 import by.alexandr7035.banking.ui.components.ScreenPreview
 import by.alexandr7035.banking.ui.components.SecondaryToolBar
+import by.alexandr7035.banking.ui.components.search.SearchField
 import by.alexandr7035.banking.ui.core.resources.UiText
 import by.alexandr7035.banking.ui.theme.primaryFontFamily
 
@@ -38,12 +47,37 @@ import by.alexandr7035.banking.ui.theme.primaryFontFamily
 fun HelpScreen(
     onBack: () -> Unit = {}
 ) {
+    val focusManager = LocalFocusManager.current
+
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .pointerInput(Unit) {
+                detectTapGestures(onTap = {
+                    focusManager.clearFocus()
+                })
+            },
     ) {
         SecondaryToolBar(
             onBack = onBack,
             title = UiText.StringResource(R.string.help_center)
+        )
+
+        val searchQuery = rememberSaveable() {
+            mutableStateOf<String>("")
+        }
+
+        SearchField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    horizontal = 24.dp,
+                    vertical = 16.dp
+                ),
+            query = searchQuery.value,
+            onQueryChanged = {
+                searchQuery.value = it
+            }
         )
 
         val helpItems = listOf(
@@ -67,10 +101,20 @@ fun HelpScreen(
 
         LazyColumn(
             modifier = Modifier.fillMaxSize(),
-//            contentPadding = PaddingValues(horizontal = 24.dp),
-//            verticalArrangement = Arrangement.spacedBy(24.dp)
         ) {
-            items(helpItems) {
+            val filtered = if (searchQuery.value.isNotBlank()) {
+                helpItems.filter {
+                    val text = it.text.uppercase()
+                    val title = it.title.uppercase()
+                    val query = searchQuery.value.uppercase()
+                    text.contains(query) || title.contains(query)
+                }
+            }
+            else {
+                helpItems
+            }
+
+            items(filtered) {
                 HelpItemCard(
                     item = it,
                     expanded = expandedItem.value == it
