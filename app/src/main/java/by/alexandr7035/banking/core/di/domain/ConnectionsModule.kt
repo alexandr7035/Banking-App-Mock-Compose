5e1a54ff0b747944e57daee017e71c3fb6ed2f0d
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2023-11-21 02:58:12 +0300

    Add LoadUserFromQrCodeUseCase

--- app/src/main/java/by/alexandr7035/banking/core/di/domain/ConnectionsModule.kt
+++ app/src/main/java/by/alexandr7035/banking/core/di/domain/ConnectionsModule.kt
@@ -1,8 +1,12 @@
 package by.alexandr7035.banking.core.di.domain
 
+import by.alexandr7035.banking.domain.features.connections.LoadUserFromQrCodeUseCase
 import by.alexandr7035.banking.domain.features.qr_codes.GenerateQrCodeUseCase
 import org.koin.dsl.module
 
 val connectionsModule = module {
     factory { GenerateQrCodeUseCase() }
+    factory { LoadUserFromQrCodeUseCase(
+        contactsRepository = get()
+    ) }
 }
