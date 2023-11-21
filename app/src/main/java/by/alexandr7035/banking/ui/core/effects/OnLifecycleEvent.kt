86a468dd3d609244fefe61b6b95e856c86cb1d2b
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2023-11-21 16:11:21 +0300

    Add OnLifecycleEvent

---
+++ app/src/main/java/by/alexandr7035/banking/ui/core/effects/OnLifecycleEvent.kt
@@ -0,0 +1,28 @@
+package by.alexandr7035.banking.ui.core.effects
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
+
+// https://stackoverflow.com/a/66807899/16720812
+@Composable
+fun OnLifecycleEventEffect(onEvent: (owner: LifecycleOwner, event: Lifecycle.Event) -> Unit) {
+    val eventHandler = rememberUpdatedState(onEvent)
+    val lifecycleOwner = rememberUpdatedState(LocalLifecycleOwner.current)
+    
+    DisposableEffect(lifecycleOwner.value) {
+        val lifecycle = lifecycleOwner.value.lifecycle
+        val observer = LifecycleEventObserver { owner, event ->
+            eventHandler.value(owner, event)
+        }
+
+        lifecycle.addObserver(observer)
+        onDispose {
+            lifecycle.removeObserver(observer)
+        }
+    }
+}
