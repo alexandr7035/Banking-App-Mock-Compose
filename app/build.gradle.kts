5ee19ed58fa691e428344fb8f407e3bd596beb9d
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2025-05-22 12:49:22 +0200

    Migrate to version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,13 @@
-import java.util.Properties
 import java.io.FileInputStream
+import java.util.Properties
 
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.devtools.ksp")
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.20"
+    with(libs.plugins) {
+        alias(android.application)
+        alias(kotlin.android)
+        alias(compose.compiler)
+        alias(ksp)
+    }
 }
 
 val keystorePropertiesFile = rootProject.file("keystore.properties")
@@ -65,74 +67,74 @@ android {
         compose = true
         buildConfig = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.6"
-    }
+
     packaging {
         resources.excludes.add("/META-INF/{AL2.0,LGPL2.1}")
     }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
-    implementation("androidx.activity:activity-compose:1.10.1")
-    implementation(platform("androidx.compose:compose-bom:2025.05.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
+    with(libs) {
 
-    implementation("androidx.compose.material3:material3:1.3.2")
+        // Android core dependencies
+        implementation(androidx.core.ktx)
+        implementation(androidx.ui)
+        implementation(androidx.material3)
+        implementation(androidx.core.splashscreen)
+        implementation(androidx.work.runtime.ktx)
 
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2025.05.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+        // SL
+        implementation(bundles.koin)
 
-    val koinVersion = "4.0.4"
-    implementation("io.insert-koin:koin-androidx-compose:$koinVersion")
-    implementation("io.insert-koin:koin-androidx-workmanager:$koinVersion")
+        // Images
+        implementation(bundles.coil)
 
-    implementation("androidx.compose.ui:ui-text-google-fonts:1.8.1")
+        // Room
+        implementation(bundles.room)
+        ksp(room.compiler)
 
-    val navVersion = "2.9.0"
-    implementation("androidx.navigation:navigation-compose:$navVersion")
-    implementation("com.github.leonard-palm:compose-state-events:2.1.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.9.0")
+        // Lifecycle
+        implementation(lifecycle.runtime.compose)
+        implementation(lifecycle.runtime.ktx)
 
-    implementation("com.github.cioccarellia:ksprefs:2.4.1")
+        // Compose
+        implementation(platform(compose.bom))
+        implementation(compose.activity)
+        implementation(compose.shimmer)
+        implementation(compose.state.events)
+        implementation(compose.navigation)
+        implementation(compose.graphics)
+        implementation(compose.tooling.preview)
+        implementation(compose.fonts)
+        implementation(compose.paging)
+        implementation(compose.constraintlayout)
 
-    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.1")
+        // Security
+        implementation(androidx.security.crypto)
+        implementation(androidx.biometric.ktx)
 
-    val coilVersion = "2.6.0"
-    implementation("io.coil-kt:coil-compose:$coilVersion")
-    implementation("io.coil-kt:coil-svg:$coilVersion")
+        // Utils
+        implementation(permissionx)
+        implementation(ksprefs)
 
-    implementation("com.valentinilk.shimmer:compose-shimmer:1.3.2")
+        // QR code
+        with(zxing) {
+            //noinspection GradleDependency
+            implementation(core)
+            implementation(android) {
+                isTransitive = false
+            }
+        }
 
-    val roomVersion = "2.7.1"
-    implementation("androidx.room:room-runtime:$roomVersion")
-    implementation("androidx.room:room-ktx:$roomVersion")
-    ksp("androidx.room:room-compiler:$roomVersion")
+        // Test dependencies
+        testImplementation(junit)
+        androidTestImplementation(androidx.junit)
+        androidTestImplementation(androidx.espresso.core)
+        androidTestImplementation(platform(compose.bom))
+        androidTestImplementation(androidx.ui.test.junit4)
 
-    implementation("androidx.core:core-splashscreen:1.0.1")
-
-    implementation("androidx.security:security-crypto:1.1.0-alpha07")
-    implementation("androidx.biometric:biometric-ktx:1.2.0-alpha05")
-
-    implementation("androidx.paging:paging-compose:3.3.6")
-
-    val workVersion = "2.10.1"
-    implementation("androidx.work:work-runtime-ktx:$workVersion")
-
-    implementation("com.guolindev.permissionx:permissionx:1.7.1")
-
-    implementation("com.journeyapps:zxing-android-embedded:4.3.0") {
-        isTransitive = false
+        // Debug dependencies
+        debugImplementation(compose.tooling)
+        debugImplementation(androidx.ui.test.manifest)
     }
-    //noinspection GradleDependency
-    implementation("com.google.zxing:core:3.3.0")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.9.3" apply false
-    id("com.android.library") version "8.9.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
-    id("com.google.devtools.ksp") version "2.1.20-1.0.31" apply false
+    with(libs.plugins) {
+        alias(android.application) apply false
+        alias(android.library) apply false
+        alias(ksp) apply false
+        alias(kotlin.android) apply false
+    }
 }
